5e832459daa68acfffb9f3d326f53acdc6ef7d1e
flavws <persona.fmaria@gmail.com>
2022-10-13 19:04:02 -0300

    Initial commit

---
+++ src/test/java/br/com/transacoes/transacoes/TransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.transacoes.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
